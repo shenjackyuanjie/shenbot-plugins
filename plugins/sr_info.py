9acd42766b87b9b1fcdfc55024b0b5de910dd69a
shenjack <3695888@qq.com>
2024-11-30 20:38:01 +0800

    给 sr 加个解析飞船的功能

--- plugins/sr_info.py
+++ plugins/sr_info.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import re
+
 import requests
 
 
@@ -17,6 +19,8 @@ LAST_SHIP_CMD = f"{CMD_PREFIX} last ship"
 LAST_SAVE_CMD = f"{CMD_PREFIX} last save"
 INFO_CMD = f"{CMD_PREFIX} info" # info xxxxx(int)
 
+# http://jundroo.com/ViewShip.html?id=1323466
+SHIP_URL_PREFIX = "http://jundroo.com/ViewShip.html?id="
 
 def data_type_fmt(data_type: str) -> str:
     if data_type == "ship":
@@ -86,13 +90,26 @@ def get_ship_info(msg: IcaNewMessage, client: IcaClient):
     client.send_message(msg.reply_with(formatted))
 
 
-def on_ica_message(msg: IcaNewMessage, client: IcaClient) -> None:
-    if msg.is_from_self or not msg.is_room_msg:
-        return
-    
-    if not msg.content.startswith(CMD_PREFIX):
-        return
-    
+NUM_PATTERN = re.compile(r'(\d+)')
+
+
+def parse_url(url: str) -> int | None:
+    """解析 url
+    找到连续的前几个数字
+
+    Args:
+        url (str): 123123xxxxxxx
+
+    Returns:
+        int | None: 解析完的 id
+    """
+    match = re.match(r'(\d+)', url)
+    if match:
+        return int(match.group(1))
+    return None
+
+
+def handle_command(msg: IcaNewMessage, client: IcaClient) -> None:
     if msg.content == LAST_SHIP_CMD:
         client.send_message(msg.reply_with(last_data("ship")))
     elif msg.content == LAST_SAVE_CMD:
@@ -101,3 +118,38 @@ def on_ica_message(msg: IcaNewMessage, client: IcaClient) -> None:
         client.send_message(msg.reply_with(last_data("data")))
     elif msg.content.startswith(INFO_CMD):
         get_ship_info(msg, client)
+
+
+def handle_url(msg: IcaNewMessage, client: IcaClient) -> None:
+    # 找到所有的飞船链接
+    urls = msg.content.split(SHIP_URL_PREFIX)
+    # 因为已经判断过有了, 所以 len >= 2
+    urls.pop(0)
+    vaild_urls = [url for url in map(parse_url, urls) if url is not None]
+    # 去重
+    vaild_urls = list(set(vaild_urls))
+    if len(vaild_urls) == 0:
+        return
+    get_infos = []
+    for ship_id in vaild_urls:
+        try:
+            res = requests.get(f"{API_URL}/info/{ship_id}", timeout=5)
+            data = res.json()
+        except (requests.RequestException, requests.Timeout) as e:
+            get_infos.append(f"请求中出现问题: {e} {res}")            
+        if data["code"] != 200:
+            get_infos.append(f"请求失败: {data['msg']}")
+        fmt_msg = f"ID: {data['data']['save_id']}-长度: {format_data_size(data['data']['len'])}\nblake3 hash: {data['data']['blake_hash']}"
+        get_infos.append(fmt_msg)
+    client.send_message(msg.reply_with("\n\n".join(get_infos)))
+        
+
+def on_ica_message(msg: IcaNewMessage, client: IcaClient) -> None:
+    if msg.is_from_self or not msg.is_room_msg:
+        return
+    
+    if msg.content.startswith(CMD_PREFIX):
+        return handle_command(msg, client)
+
+    if SHIP_URL_PREFIX in msg.content:
+        handle_url(msg, client)
